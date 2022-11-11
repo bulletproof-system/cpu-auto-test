cf9771775d79ee7d19847c859264fde6aecfecfb
LTT <1728901409@qq.com>
2022-11-11 15:49:07 +0800

    example

--- example/test_pipeline.bat
+++
@@ -1,4 +0,0 @@
-cd ..\\
-python auto_test.py -f .\\example\\test_pipeline.asm --test .\\example\\mips.v --debug --delay-enbled
-@REM 使用 --debug 以输出调试信息
-pause
--- example/test_single_cycle.bat
+++
@@ -1,4 +0,0 @@
-cd ..\\
-python auto_test.py -f .\\example\\test_single_cycle.asm --test .\\example\\mips.v --debug
-@REM 使用 --debug 以输出调试信息
-pause
